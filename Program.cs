8a0b3f9de683211f6a29c8a16eba190bd0fdec0b
Afag Turhan <117473262+marfusha1986@users.noreply.github.com>
2026-06-03 08:41:45 +0300

    Introduce inner block with variable and output
    Added inner block with a variable and updated output statements.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int disBlokSayi = 10;
+            { // ic blok aciliyor
+                int icBlokSayi = 5;
+                Console.WriteLine("Dış bloktaki degisken: {0}", disBlokSayi);
+                Console.WriteLine("İç bloktaki degisken: {0}", icBlokSayi);
+            } // ic blok burada bitiyor
+            Console.WriteLine("Dış bloktaki degisken: {0}", disBlokSayi);
+           // Console.WriteLine("İç bloktaki degisken: {0}", icBlokSayi); // hata
+            Console.ReadLine();
         }
     }
 }
