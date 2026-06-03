163eddff13646ced48959ce0a370a5a886a402fc
afaq imanova <afaq@imanov.name>
2026-06-03 08:31:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BlokSeviyesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
